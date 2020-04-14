c26680bb2ab3aa0fe32972dff2f670e51da6116c
koualsky <koualsky@gmail.com>
2020-04-14 19:05:50 +0200

    Add imports

---
+++ imports/main.py
@@ -0,0 +1,15 @@
+"""
+__init__.py   <-   Niepotrzebne od Python 3.3+
+Wcześniej potrzebne było abym mógł zrobić:
+
+import package.module
+
+OR
+
+from package import module
+
+"""
+
+from package import module
+
+human = module.Human()
