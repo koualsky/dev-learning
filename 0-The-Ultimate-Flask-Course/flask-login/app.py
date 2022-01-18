a709869a351a466bb4f01e5b05ab0cbbc37a847c
koualsky <koualsky@gmail.com>
2022-01-18 18:04:09 +0100

    Added User model

--- 0-The-Ultimate-Flask-Course/flask-login/app.py
+++ 0-The-Ultimate-Flask-Course/flask-login/app.py
@@ -1,12 +1,32 @@
 from flask import Flask
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin
+from flask_sqlalchemy import SQLAlchemy
 
 login_manager = LoginManager()
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True)
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     login_manager.init_app(app)
+    db.init_app(app)
 
     return app
+
+
+# How to create db? -> In activated venv in shell:
+# from app import db, create_app, User
+# db.create_all(app=create_app())
+# fin :)
+# exit()
+# sqlite3 db.sqlite3
+# .tables (and i should see my tables)
+# .schema -> will show me schema of user model
